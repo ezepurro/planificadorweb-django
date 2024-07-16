397f3919a8ff3d547db463a8305afd4d8b02ce3e
Ezequiel Purro <ezequiel.purro@gmail.com>
2024-07-16 12:43:01 -0300

    Finalización form add_task

--- planificadorweb/tasks/urls.py
+++ planificadorweb/tasks/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import Dashboard, TaskDetail
+from .views import Dashboard, TaskDetail, add_task
 
 urlpatterns = [
     path('dashboard/', Dashboard.as_view(), name="dashboard"),
-    path('add/', views.add_task, name="add_task"),
+    path('add/', add_task, name="add_task"),
     path('detail/<int:pk>/', TaskDetail.as_view(), name="task_detail"),
 
 ]
--- planificadorweb/tasks/views.py
+++ planificadorweb/tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
 from .models import Task
 from .forms import TaskForm
 
@@ -13,14 +15,12 @@ def dashboard(request):
 
 # CLASS-BASED VIEW
 class Dashboard(ListView):
-    # template_name = "tasks/dashboard.html"
     model = Task
 
+    #Si fuera un simple templateview, de esta forma se pasa el diccionario de contexto
     # def get(self, request, *args, **kwargs):
         # tasks = Task.objects.all()
         # return render(request, self.template_name, {'tasks':tasks})
-
-# ---
 
 
 class TaskDetail(DetailView):
@@ -33,11 +33,17 @@ def add_task(request):
     if request.method == "POST":
         task_form = TaskForm(data=request.POST)
         if task_form.is_valid():
-            title = request.POST.get('title', '')
-            description = request.POST.get('description', '')
-            expiration = request.POST.get('expiration', '')
-            print(expiration)
+            current_title = request.POST.get('title', '')
+            current_description = request.POST.get('description', '')
+            current_expiration = request.POST.get('expiration', '')
+            # Cambiar target al usuario correcto
+            current_target = request.user
+            current_author = request.user
+            print("Title:", current_title, "Description:", current_description, "Expiration:", current_expiration, "Target:", current_target, "Author:", current_author)
+            new_task = Task(title = current_title, description = current_description, author = current_author, target = current_target, expiration = current_expiration)
+            new_task.save()
             return redirect('../dashboard/')
 
     return render(request, "tasks/add_task.html", {'form': task_form})
+
 
