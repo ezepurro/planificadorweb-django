c02d790f94168877f698e69ddd5cd55799310b7b
Ezequiel Purro <ezequiel.purro@gmail.com>
2024-07-22 20:41:00 -0300

    Eliminar usuario terminado

--- planificadorweb/registration/urls.py
+++ planificadorweb/registration/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AddMember, ProfileUpdate, MemberDetail, logoutexp
+from .views import AddMember, ProfileUpdate, MemberDetail, logoutexp, UserDeleteView
 
 urlpatterns = [
     path('add-member/', AddMember.as_view(), name="add_member"),
     path('profile-update/', ProfileUpdate.as_view(), name="profile_form"),
     path('member-detail/<int:pk>/', MemberDetail.as_view(), name="member_detail"),
+    path('delete/<int:pk>/', UserDeleteView.as_view(), name="user_delete"),
     path('logoutexp/', logoutexp, name="logoutexp"),
 ]
